5a8cb0443ae78ac7a9621169ec4416e97b8ea319
Vaishnavi Chirmade <vaishnavi.chirmade@gmail.com>
2024-11-09 18:30:44 +0530

    EOS_Assignments

---
+++ Assignment_08/assignment01.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+void sigint_handler(int sig) 
+{
+    printf("Interrupted Continues...\n");
+}
+
+int main() 
+{
+    char cmd[512], *ptr, *args[32];
+    int i, ret, err, s;
+
+    signal(SIGINT, sigint_handler);
+
+	while(1) 
+	{
+        printf("cmd> ");
+        gets(cmd);
+        i = 0;
+        ptr = strtok(cmd, " ");
+        args[i] = ptr;
+		i++;
+
+        do 
+		{
+            ptr = strtok(NULL, " ");
+            args[i] = ptr;
+			i++;
+        }
+		while(ptr != NULL);
+
+        if(strcmp(args[0], "exit") == 0)
+            _exit(0);
+
+        else if(strcmp(args[0], "cd") == 0)
+            chdir(args[1]);
+
+        else 
+		{
+            ret = fork();
+            if(ret == 0) 
+			{
+                err = execvp(args[0], args);
+                if(err < 0) 
+				{
+                    perror("exec() failed");
+                    _exit(1);
+                }
+            }
+            else
+                waitpid(-1, &s, 0);
+        }
+    }
+
+    return 0;
+
+}
